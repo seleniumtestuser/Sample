cd0582a77e559804a8857a426d039fd9908ead89
Karthikeyan <Karthikeyan@Karthik>
2020-01-28 21:47:46 +1100

    Java Version One

---
+++ Sample/src/test/java/sampleOne/Sample/javaone.java
@@ -0,0 +1,11 @@
+package sampleOne.Sample;
+
+public class javaone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Version one");
+
+	}
+
+}
